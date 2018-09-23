543b116c23c60eb50d1e337b5cb243cce3a7cf79
Nick Blackburn <kritz117@gmail.com>
2018-09-24 01:01:57 +1000

    Create ActionBehaviours.cs

---
+++ Code/ActionBehaviours.cs
@@ -0,0 +1,51 @@
+/* Author: Nicholas Blackburn
+*
+* A small example of designer-friendly code, created to look nice in Unity's inspector and provide helpful tooltips as to functionality.
+*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class ActionBehaviours
+{
+	public enum AnimationType
+	{
+		None,
+		PickupCrouch,
+		PickupStanding
+	}
+	
+  [Header("Action Behaviours")]
+	[Tooltip("Makes GameObject disappear once action has been performed.")]
+	public bool HideObject = false;
+	[Tooltip("Determines what animation the player should do when this action is performed.")]
+	public AnimationType PlayerAnimation;
+	[Tooltip("Runs a Yarn node action is activated.")]
+	[FormerlySerializedAs("DialogueNodeStart")]
+	public string YarnNode;
+	[Tooltip("Launches the Inspection window.")]
+	[FormerlySerializedAs("InspectName")]
+	public string InspectWindow;
+	[Tooltip("Loads a scene when action is activated.")]
+	public string SceneToLoad;
+
+    [Header("Events")]
+    public UnityEvent OnUse;
+
+	[Header("Advanced Settings")]
+	[Tooltip("Only perform this action if these action names have already been performed. NOTE: Keep action names unique!")]
+    public List<string> PrerequisiteActions = new List<string>();   // ActionNames of other actions that need to be found in ActionTrackingManager before this action is useable.
+	[Tooltip("Allows action to be performed only once")]
+	public bool ActionIsOnceOff = false;								// Defines if this action can only be called once (e.g., smashing a vase) or multiple times (e.g., talking to someone)
+	[Tooltip("Hides action from appearing in game UI menus.")]
+	public bool ActionIsHidden = false;
+	[Tooltip("How long does this object take to hide after the action is called?")]
+	public float HideDelay = 2f;
+
+    public ActionBehaviours(){}
+}
